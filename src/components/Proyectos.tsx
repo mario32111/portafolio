8caaf37c67834b46142c02b4fc2049dc44ce8a69
mario32111 <marioge44@gmail.com>
2025-05-16 20:14:59 -0600

    repositioros

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -15,68 +15,69 @@ const Proyectos: FunctionComponent<ProyectosProps> = () => {
     {
       title: "HydroLink – Sistema de riego inteligente",
       description: "Desarrollo de un sistema automatizado de riego que ajusta los ciclos de riego en función de los datos recolectados por sensores de humedad y temperatura del suelo. Permite una gestión eficiente del agua mediante controladores con conectividad móvil que transmiten la información en tiempo real. Incluye procesamiento y análisis de datos en tiempo real para optimizar el riego, y una arquitectura distribuida con aplicación móvil, llaves inteligentes de campo, y API REST alojada en AWS. Se implementaron flujos de CI/CD.",
-      technologies: ["React Native", "MQTT", "TypeScript", "JavaScript", "PostgreSQL", "Express", "Sequelize", "C++", "Docker", "CI/CD", "AWS"],
+      technologies: ["React Native", "Docker" , "JWT" , "Redux", "MQTT", "TypeScript", "JavaScript", "PostgreSQL", "Express", "Sequelize", "C++", "Docker", "CI/CD", "AWS"],
       imageUrl: p2,
       // Múltiples repositorios de GitHub
       githubUrls: [
-        { name: "Backend", url: "https://github.com/mario32111/backend-hydrolink" },
-        { name: "App Móvil", url: "https://github.com/mario32111/Hydrolink-App" },
-        { name: "Dispositivo IoT", url: "https://github.com/mario32111/hydrolink-IoT-Firmware" },
+        { name: "Backend", url: "https://github.com/mario32111/api-pub-sub" },
+        { name: "App Móvil", url: "https://github.com/mario32111/hydro-app" },
+        { name: "Dispositivo IoT", url: "https://github.com/mario32111/HydrolinkArduino" },
       ],
       liveUrl: null,
     },
     {
       title: "ProyectoExpotec – Aplicación web de preguntas sobre informática",
-      description: "Aplicación web interactiva y llamativa desarrollada para la Expotec, con el objetivo de crear una experiencia divertida para estudiantes de preparatoria. Permite realizar preguntas aleatorias sobre informática y visualizar los resultados en tiempo real. Manejó alrededor de 50 usuarios activos simultáneamente y fue desarrollado en una semana, recibiendo una excelente acogida por su interfaz amigable y dinámica.",
+      description: "Aplicación web interactiva y llamativa desarrollada para un evento de la universidad, con el objetivo de crear una experiencia divertida para estudiantes de preparatoria. Permite realizar preguntas aleatorias sobre informática y visualizar los resultados en tiempo real. Manejó alrededor de 50 usuarios activos simultáneamente y fue desarrollado en una semana, recibiendo una excelente acogida por su interfaz amigable y dinámica.",
       technologies: ["React", "TypeScript", "PostgreSQL", "Redux", "Express", "Sequelize", "HTML", "CSS", "Render"],
       imageUrl: p1,
       // Un solo repositorio, pero sigue siendo un array
       githubUrls: [
-        { name: "Repositorio", url: "https://github.com/mario32111/expotec-app" },
+        { name: "Pagina web", url: "https://github.com/mario32111/ProyectoExpotec-Api" },
+        { name: "API", url: "https://github.com/mario32111/ProyectoExpotec-Api"}
       ],
       liveUrl: "https://expotec.onrender.com/", // Reemplaza con la URL real de tu proyecto desplegado
     },
     {
       title: "KapraDesign – Sistema de seguimiento de pedidos y procesos",
-      description: "Sistema desarrollado para plataformas móviles y PC con el objetivo de mostrar y controlar el flujo de trabajo de la empresa, ligado directamente a sus pedidos. Fue desarrollado de forma freelance, incluyendo análisis de necesidades e interacción directa con el cliente. El sistema está conformado por una aplicación móvil, una API REST, una base de datos y una aplicación de escritorio. Aunque no fue implementado comercialmente, sirvió como prueba de concepto y tiene alta posibilidad de implementarse en futuro.",
-      technologies: ["React Native", "TypeScript", "PostgreSQL", "Sequelize", "Express", "Electron", "Docker"],
+      description: "Sistema desarrollado para plataformas móviles con el objetivo de mostrar y controlar el flujo de trabajo de la empresa, ligado directamente a sus pedidos. Fue desarrollado de forma freelance, incluyendo análisis de necesidades e interacción directa con el cliente. El sistema está conformado por una aplicación móvil, una API REST, una base de datos y una aplicación de escritorio. Aunque no fue implementado comercialmente, sirvió como prueba de concepto y tiene alta posibilidad de implementarse en futuro.",
+      technologies: ["React Native", "TypeScript", "Expo"],
       imageUrl: p3,
       githubUrls: [
-        { name: "API REST", url: "https://github.com/mario32111/kapra-design-api" },
-        { name: "App Escritorio", url: "https://github.com/mario32111/kapra-design-desktop" },
-        { name: "App Móvil", url: "https://github.com/mario32111/kapra-design-mobile" },
+
+        { name: "Repositorio", url: "https://github.com/mario32111/KapraDesign" },
       ],
       liveUrl: null,
     },
     {
       title: "Microservices-Superflights",
-      description: "API REST para la gestión de vuelos, utilizando una arquitectura de microservicios. Permite a los usuarios buscar y reservar vuelos de manera eficiente, con un diseño moderno y funcional.",
+      description: "API REST para la gestión de vuelos desarrollada a lo largo de un curso, utilizando una arquitectura de microservicios. Permite a los usuarios buscar y reservar vuelos de manera eficiente, con un diseño moderno y funcional.",
       technologies: ["Nest.js", "AWS", "JWT", "Docker", "Swagger", "MongoDB", "Node.js", "GitHub", "Postman"],
       imageUrl: p3,
       githubUrls: [
-        { name: "Gateway", url: "https://github.com/mario32111/nest-superflights-api-gateway" },
-        { name: "Auth Service", url: "https://github.com/mario32111/nest-superflights-auth-service" },
-        { name: "Flights Service", url: "https://github.com/mario32111/nest-superflights-flights-service" },
+        { name: "Gateway", url: "https://github.com/mario32111/superFlights-apiGateway" },
+        { name: "Users Service", url: "https://github.com/mario32111/superFlights-microserviceUsers" },
+        { name: "Flights Service", url: "https://github.com/mario32111/superFlights-microserviceFlights" },
+        { name: "Passengers Service", url: "https://github.com/mario32111/superFlights-microserviceFlights"}
       ],
       liveUrl: null,
     },
     {
       title: "MyEconomy",
-      description: "Maquetación de una aplicación móvil para el control de gastos personales, con un diseño atractivo y funcional. La aplicación permite a los usuarios llevar un registro de sus gastos e ingresos, ayudando a gestionar su economía personal de manera eficiente.",
+      description: "Maquetación de una aplicación web para el control de gastos personales, con un diseño atractivo y funcional. La aplicación permite a los usuarios llevar un registro de sus gastos e ingresos, ayudando a gestionar su economía personal de manera eficiente.",
       technologies: ["React Native", "React", "JavaScript", "CSS"],
       imageUrl: p3,
       githubUrls: [
-        { name: "Repositorio", url: "https://github.com/mario32111/my-economy-app" },
+        { name: "Repositorio", url: "https://github.com/mario32111/MyEconomy" },
       ],
       liveUrl: null,
     },
     {
       title: "ArtChallange",
-      description: "Aplicación web para la creación de arte digital, con una interfaz intuitiva y herramientas avanzadas para artistas. Permite a los usuarios crear, editar y compartir sus obras de arte en una comunidad en línea.",
-      technologies: ["Angular", "Firebase", "HTML", "CSS", "TypeScript"],
+      description: "Aplicación web estilo red social orientada a retos semanales de arte, con una interfaz intuitiva y herramientas avanzadas para artistas. Permite a los usuarios crear, editar y compartir sus obras de arte en una comunidad en línea.",
+      technologies: ["Angular", "Firebase", "HTML", "CSS", "TypeScript", "Bootstrap"],
       imageUrl: p3,
       githubUrls: [
-        { name: "Repositorio", url: "https://github.com/mario32111/art-challenge-angular" },
+        { name: "Repositorio", url: "https://github.com/mario32111/ArtChallange" },
       ],
       liveUrl: null,
     },
